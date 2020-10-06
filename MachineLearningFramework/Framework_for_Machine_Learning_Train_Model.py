cb87fab615285ea60939a7fda8ec27725008f1c7
Jason Shipp <jason.shipp1989@hotmail.co.uk>
2020-10-06 15:47:29 +0100

    Update Framework_for_Machine_Learning_Train_Model.py

--- MachineLearningFramework/Framework_for_Machine_Learning_Train_Model.py
+++ MachineLearningFramework/Framework_for_Machine_Learning_Train_Model.py
@@ -124,7 +124,7 @@ train_data_variables_no_upsampling = train_data[train_data[outcome_variables[0]]
 train_data_variables_upsampling = train_data[train_data[outcome_variables[0]].apply(lambda x: x in outcome_variable_values_to_upsample)]
 
 if len(outcome_variable_values_to_upsample) > 0:
-    train_data_variables_upsampled = train_data_variables_upsampling.sample(n = len(train_data_variables_no_upsampling) * upsample_proportion_of_common_variable, replace = True)
+    train_data_variables_upsampled = train_data_variables_upsampling.sample(n = int(np.ceil(len(train_data_variables_no_upsampling) * upsample_proportion_of_common_variable)), replace = True)
     train_data = pd.concat([train_data_variables_no_upsampling, train_data_variables_upsampled], axis = 0)
 
 print('Training data after up-sampling outcome variable:')
